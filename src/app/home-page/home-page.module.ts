75288bdc6e7c0f192f608fae0866244a24d42528
muthukumar4999 <muthukumar4999@gmail.com>
2019-02-01 00:47:59 +0530

    UI outline made

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule,MatFormFieldModule, MatInputModule, MatIconModule, MatTableModule} from '@angular/material';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
@@ -10,7 +12,13 @@ import { HomePageComponent } from './home-page.component';
   ],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule
   ]
 })
 export class HomePageModule { }
