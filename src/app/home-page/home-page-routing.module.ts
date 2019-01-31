419ebc1bedfcfc5319cea7783cb4ec8543f9f4ec
sandeep.l <sandeep.l@gofrugal.com>
2019-01-31 16:43:36 +0530

    routing added

--- src/app/home-page/home-page-routing.module.ts
+++ src/app/home-page/home-page-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page.component';
 
 const routes: Routes = [
   {
     path: '**',
-    loadChildren: './home-page/home-page.module#HomePageModule'
-  }
+    component: HomePageComponent
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes , { useHash: true })],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomePageRoutingModule { }
